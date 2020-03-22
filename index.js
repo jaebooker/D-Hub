50541155dbe21f649e0dfef5fb0e37644eb4e7f2
jaebooker <jaesonbooker@gmail.com>
2020-03-22 17:15:09 -0400

    changes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-const ThreeBox = require('./services/3box.js');
+const ThreeBox = require('./services/3Box.js');
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
--- services/3box.js
+++ services/3box.js
@@ -1,34 +1,34 @@
 const Box = require('3box');
 const Infura = require('./infura.js');
-const myProfile = await Box.getProfile(myAddress);
+const myProfile = Box.getProfile(myAddress);
 const {
   profileGraphQL, getProfile,
   getProfiles, getVerifiedAccounts
 } = require('3box/lib/api');
 
-const box = await Box.openBox(myAddress, window.ethereum, {});
-const space = await box.openSpace('myApp');
+const box = Box.openBox(myAddress, window.ethereum, {});
+const space = box.openSpace('myApp');
 
 const BoxData = async (name) => {
-    const hash = await space.public.get(name);
-    const infuraData = await Infura.IPFSPortalGet.getObjectData(hash);
+    const hash = space.public.get(name);
+    const infuraData = Infura.IPFSPortalGet.getObjectData(hash);
     return infuraData;
 };
 
 const AllBoxData = async => {
-    const boxData = await space.public.all();
+    const boxData = space.public.all();
     let infuraDataArray;
     boxData.forEach(function(hash,index) {
-        const infuraData = await Infura.IPFSPortalGet(hash);
+        const infuraData = Infura.IPFSPortalGet(hash);
         infuraDataArray.push(infuraData);
     });
     return infuraDataArray;
 };
 
 const CreateBoxData = async (name, object) => {
-    const infuraData = await Infura.IPFSPortalPost(object);
+    const infuraData = Infura.IPFSPortalPost(object);
     const hash = infuraData.Hash;
-    const infuraHash = await box.public.set(name, hash);
+    const infuraHash = box.public.set(name, hash);
     return infuraHash;
 };
 
