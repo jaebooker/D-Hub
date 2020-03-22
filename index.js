0e94c8c565aab6ba1c92c250a876d08104237f75
jaebooker <jaesonbooker@gmail.com>
2020-03-21 20:15:43 -0400

    added things

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-const ThreeBox = require('.services/infura.js');
+const ThreeBox = require('./services/3Box.js');
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .get('/', (req, res) => {
+      infuraData = ThreeBox.BoxData.getAllBoxData();
+      res.render('pages/index', infuraData)});
   .post('/', (req, res) => {
     ThreeBox.CreateBoxData.setBoxData(req.title, req.story);
     res.send(`Your story was successfully saved to the ipfs!`);
